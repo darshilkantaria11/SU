48b9a03d850311261db459e017f3b65a744799f1
darshilkantaria11 <darshilkantaria11@gmail.com>
2023-04-30 14:54:20 +0530

    navbar_dropdown

--- codeeasy/components/Navbar.js
+++ codeeasy/components/Navbar.js
@@ -2,8 +2,25 @@ import React from 'react'
 import { MdEditSquare } from "react-icons/md";
 import { BsArrowRight } from "react-icons/bs";
 import Link from 'next/link';
+import { useState } from 'react';
+
 
 const Navbar = () => {
+  const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
+
+  const toggleDropdown1 = () => {
+    setIsDropdownOpen(false);
+    setIsDropdownOpen1(!isDropdownOpen1); 
+  };
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen1(false);
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+
   return (
     <div>
       <header className="text-black-100 body-font border-b">
@@ -12,16 +29,54 @@ const Navbar = () => {
             <MdEditSquare size={32} />
             <span className=" text-2xl">SquareEdits</span>
           </a>
-          <nav className="md:mr-auto md:ml-20 md:py-1   md:pl-4  text-black- flex flex-wrap items-center text-base justify-between  ">
+          <nav className="md:mr-auto md:ml-20 md:py-1 md:pl-4 text-black- flex flex-wrap items-center text-base justify-between">
             <Link className="mr-5 text-xl hover:text-blue-500 " href={'/'}> Home</Link>
-            <Link className="mr-5 text-xl hover:text-blue-500 " href={'/'}> Templates</Link>
-            <Link className="mr-5 text-xl hover:text-blue-500 " href={'/'}> Components</Link>
-
-
+            <div className="relative">
+              <button
+                className="mr-5 text-xl hover:text-blue-500 focus:outline-none"
+                onClick={toggleDropdown1}
+              >
+                Templates
+              </button>
+              {isDropdownOpen1 && (
+                <div className="absolute right-0 z-10 mt-2 py-2 w-48 bg-white rounded-md shadow-xl">
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 1
+                  </Link>
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 2
+                  </Link>
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 3
+                  </Link>
+                </div>
+              )}
+            </div>
+            <div className="relative">
+              <button
+                className="mr-5 text-xl hover:text-blue-500 focus:outline-none"
+                onClick={toggleDropdown}
+              >
+                Components
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute right-0 z-10 mt-2 py-2 w-48 bg-white rounded-md shadow-xl">
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 1
+                  </Link>
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 2
+                  </Link>
+                  <Link className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" href="#">
+                    Template 3
+                  </Link>
+                </div>
+              )}
+            </div>
           </nav>
           <div className="flex items-center border rounded-full">
-            <input className="py-2 px-3 rounded-full focus:outline-none" type="text" placeholder="Search..."/>
-              <button className=" py-2 px-4 "><BsArrowRight size={30} /></button>
+            <input className="py-2 px-3 rounded-full focus:outline-none" type="text" placeholder="Search..." />
+            <button className=" py-2 px-4 "><BsArrowRight size={30} /></button>
           </div>
 
         </div>
