72b49a6b919f048015e653d5f64465e313da64e1
darshilkantaria11 <darshilkantaria11@gmail.com>
2023-05-05 19:47:42 +0530

    jwt

--- codeeasy/pages/api/login.js
+++ codeeasy/pages/api/login.js
@@ -1,19 +1,22 @@
 import User from "../../models/User"
 import connectDb from "../../middleware/mongoose"
 var CryptoJS = require("crypto-js");
+var jwt = require('jsonwebtoken');
 
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
-        
+
         let user = await User.findOne({ "email": req.body.email })
-        const bytes = CryptoJS.AES.decrypt(user.password, 'mysecret123');
-        
+        const bytes = CryptoJS.AES.decrypt(user.password, "mysecret123");
+
         let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
 
         if (user) {
             if (req.body.email == user.email && req.body.password == decryptedPass) {
-                res.status(200).json({ success: true, email: user.email, name: user.name })
+                var token = jwt.sign({ success: true, email: user.email, name: user.name }, 'myjwtsecret');
+                res.status(200).json({ token })
+
             }
             else {
                 res.status(200).json({ success: false, error: "Invalid Credentials" })
